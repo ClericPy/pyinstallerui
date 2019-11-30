5bf1eab28f8919e4d2277e7dca9407eb7adb6361
clericpy <lidongone@qq.com>
2019-11-30 15:25:28 +0800

    start to dev

---
+++ pyinstallerui/core.py
@@ -0,0 +1,17 @@
+from PyInquirer import style_from_dict, Token, prompt
+
+
+class Venv(object):
+    pass
+
+
+class Pyinstaller(object):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
